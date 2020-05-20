ecee4d61ec3a7c74ca498eddd9a8cee86ed6b3f7
Marcus Pereira <marcus.work@hotmail.com>
2020-05-20 19:18:39 -0300

    Verifying if type is valid and have enough money

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -14,6 +14,13 @@ class CreateTransactionService {
   }
 
   public execute({ title, type, value }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) throw Error('Type is invalid!');
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (total < value && type === 'outcome')
+      throw Error('You dont have enough money!');
+
     const transaction = this.transactionsRepository.create({
       title,
       type,
